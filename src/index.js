c0cb5418ba1ef4ec6f84c152d1ea68bb49323c67
CelineCambier <cambier.celine2@gmail.com>
2023-05-20 22:30:43 -0700

    project restructured

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+function showDate(event) {
+        if (event.target.value.length > 0) {
+            let currentTime = moment().tz(event.target.value).format("dddd, MMMM D, YYYY h:mm a");
+            alert(`It is ${currentTime} in ${event.target.value}.`);
+        } 
+    }
+
+    let cityElement = document.querySelector("#cities");
+    cityElement.addEventListener("change", showDate);
 
 
